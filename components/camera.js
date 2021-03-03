5b2cbec992ee13efd9f910713e3b5ce287f6cb6c
Murphy Potts <murphy@heliumservices.com>
2021-03-03 12:14:43 -0500

    First commit

---
+++ components/camera.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react'
+
+
+
+const Camera = (props) => {
+
+
+    return (
+        <div>
+            Wideo
+            <video />
+        </div>
+    )
+}
+
+export default Camera
